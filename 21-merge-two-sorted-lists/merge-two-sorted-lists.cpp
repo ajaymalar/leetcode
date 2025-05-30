50bc4ad6bb2e54774f61bfa89a0319871950d0a0
Ajay Malar M <ajaymalar0601@gmail.com>
2025-05-30 22:55:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (28.52%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1==nullptr && list2==nullptr)
@@ -76,5 +76,24 @@ public:
             return list3;
         }
         return nullptr;
+    }
+};*/
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);        
+        ListNode* tail = &dummy;  
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                tail->next = list1;    
+                list1 = list1->next;  
+            } else {
+                tail->next = list2;    
+                list2 = list2->next;  
+            }
+            tail = tail->next;        
+        }
+        tail->next = (list1 != nullptr) ? list1 : list2;
+        return dummy.next;
     }
 };
